0f222ac1b43ce30b8f3640fffa60bf70cf61ecc9
aile96 <missud2@gmail.com>
2025-10-12 22:14:47 +0200

    Added docker proxy and removed k8s proxy

--- pb/scripts/03_run_k8s.sh
+++ pb/scripts/03_run_k8s.sh
@@ -18,4 +18,5 @@ else
 fi
 
 log "Running skaffold deployment..."
-skaffold run --tag "$IMAGE_VERSION" --port-forward=user "$@"
+#skaffold run --tag "$IMAGE_VERSION" --port-forward=user "$@"
+skaffold run --tag "$IMAGE_VERSION" "$@"
--- start.sh
+++ start.sh
@@ -17,9 +17,8 @@ while IFS= read -r line; do
   export "$line"
 done < "$ENV_FILE"
 
-source "${PROJECT_ROOT}/pb/scripts/00_install_deps.sh"
-source "${PROJECT_ROOT}/pb/scripts/01_kind_cluster.sh"
-source "${PROJECT_ROOT}/pb/scripts/02_docker_network.sh"
+source "${PROJECT_ROOT}/pb/scripts/01_install_deps.sh"
+source "${PROJECT_ROOT}/pb/scripts/02_kind_cluster.sh"
 source "${PROJECT_ROOT}/pb/scripts/03_run_k8s.sh"
 
 echo "Pipeline completed"
